5ec73e29888ed7b5589ee852d3e3b74e1b21f12f
Nymphe11 <166974185+Nymphe11@users.noreply.github.com>
2024-06-25 15:57:14 +0900

    noticealert commit

--- src/pages/NoticeDetail.jsx
+++ src/pages/NoticeDetail.jsx
@@ -5,76 +5,104 @@ import { useParams } from 'react-router-dom';
 import { Button, Flex, Input, message } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import FileUpload from '../components/FileUpload';
+import { Form, Alert } from 'react-bootstrap';
 
 export default function NoticeDetail() {
-    const { id } = useParams();
-    const isCreate = id === 'create';
-    const [formData, setFormData] = useState({
-        thumbnail: '',
-        title: '',
-        content: '',
-        file: null,
-    });
+  const { id } = useParams();
+  const isCreate = id === 'create';
+  const [formData, setFormData] = useState({
+    thumbnail: '',
+    title: '',
+    content: '',
+    file: null,
+  });
 
-    const onChangeForm = (name, value) => setFormData({ ...formData, [name]: value });
+  const [show, setShow] = useState(false);
 
-    const onSubmit = () => {
-        const { thumbnail, title, content, file } = formData;
-        if (!thumbnail) return message.error('공지사항 썸네일을 등록해주세요.');
-        if (!title) return message.error('공지사항 제목을 입력해주세요.');
-        if (!content) return message.error('공지사항 내용을 입력홰주세요.');
+  const backPress = () => {
+    setShow(false);
+  };
 
-        console.log('FORMDATA', formData);
-    };
+  const onChangeForm = (name, value) =>
+    setFormData({ ...formData, [name]: value });
 
-    return (
-        <article className="content-wrapper notice-detail">
-            <PageTitle title="공지사항 상세" />
+  const onSubmit = () => {
+    const { thumbnail, title, content, file } = formData;
+    if (!thumbnail) return message.error('공지사항 썸네일을 등록해주세요.');
+    if (!title) return message.error('공지사항 제목을 입력해주세요.');
+    if (!content) return message.error('공지사항 내용을 입력홰주세요.');
 
-            <Flex vertical align="start" gap={30} style={{ width: 800 }}>
-                <section className="thumbnail-img">
-                    <label htmlFor="thumbnail" className="block mb-2 text-sm font-medium text-gray-900 ">
-                        공지사항 썸네일
-                    </label>
-                    <ImageUpload
-                        image={formData.thumbnail}
-                        handleImage={(value) => onChangeForm('thumbnail', value)}
-                        width={320}
-                        height={180}
-                    />
-                </section>
+    setShow(true);
 
-                <section style={{ width: '100%' }}>
-                    <label>공지사항 제목</label>
-                    <Input
-                        value={formData.title}
-                        name="title"
-                        onChange={(e) => onChangeForm('title', e.target.value)}
-                        placeholder="공지사항 제목을 입력해주세요"
-                    />
-                </section>
+    console.log('FORMDATA', formData);
+  };
 
-                <section style={{ width: '100%' }}>
-                    <label>공지사항 내용</label>
-                    <TextArea
-                        value={formData.content}
-                        name="content"
-                        onChange={(e) => onChangeForm('content', e.target.value)}
-                        placeholder="공지사항 내용을 입력해주세요"
-                        style={{ height: 500 }}
-                    />
-                </section>
+  return (
+    <article className="content-wrapper notice-detail">
+      <PageTitle title="공지사항 상세" />
+      <Alert show={show} variant="success">
+        <p>공지사항에 글이 등록되었습니다!</p>
+        <Button variant="secondary" size="sm" onClick={backPress}>
+          확인
+        </Button>
+      </Alert>
 
-                <section>
-                    <label>첨부파일</label>
+      <Flex vertical align="start" gap={30} style={{ width: 800 }}>
+        <section className="thumbnail-img">
+          <label
+            htmlFor="thumbnail"
+            className="block mb-2 text-sm font-medium text-gray-900 "
+          >
+            공지사항 썸네일
+          </label>
+          <ImageUpload
+            image={formData.thumbnail}
+            handleImage={(value) => onChangeForm('thumbnail', value)}
+            width={320}
+            height={180}
+          />
+        </section>
 
-                    <FileUpload file={formData.file} setValue={(value) => onChangeForm('file', value)} />
-                </section>
+        <section style={{ width: '100%' }}>
+          <label>공지사항 제목</label>
+          <Input
+            value={formData.title}
+            name="title"
+            onChange={(e) => onChangeForm('title', e.target.value)}
+            placeholder="공지사항 제목을 입력해주세요"
+          />
+        </section>
 
-                <Button htmlType="submit" size="large" type="primary" style={{ width: '100%' }} onClick={onSubmit}>
-                    {isCreate ? '공지사항 등록하기' : '공지사항 수정하기'}
-                </Button>
-            </Flex>
-        </article>
-    );
+        <section style={{ width: '100%' }}>
+          <label>공지사항 내용</label>
+          <TextArea
+            value={formData.content}
+            name="content"
+            onChange={(e) => onChangeForm('content', e.target.value)}
+            placeholder="공지사항 내용을 입력해주세요"
+            style={{ height: 500 }}
+          />
+        </section>
+
+        <section>
+          <label>첨부파일</label>
+
+          <FileUpload
+            file={formData.file}
+            setValue={(value) => onChangeForm('file', value)}
+          />
+        </section>
+
+        <Button
+          htmlType="submit"
+          size="large"
+          type="primary"
+          style={{ width: '100%' }}
+          onClick={onSubmit}
+        >
+          {isCreate ? '공지사항 등록하기' : '공지사항 수정하기'}
+        </Button>
+      </Flex>
+    </article>
+  );
 }
